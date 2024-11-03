35acdedb5356ac419f44fee281263d2a2bebfe3b
Juan Jose Garcia Sanchez <josejjgs1923@gmail.com>
2024-11-02 22:56:53 -0500

    Eliminar el texto de la licencia del theme_chooser.sh de oh-my-zsh. Este script es derivativo, expandiendo a modificar al tema de la terminal y fuente de letra.

--- tema.zsh
+++ tema.zsh
@@ -1,12 +1,5 @@
 #!/bin/zsh
 
-# Zsh Theme Chooser by fox (fox91 at anche dot no)
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
-# To Public License, Version 2, as published by Sam Hocevar. See
-# http://www.wtfpl.net/txt/copying/ for more details.
-
 _pwd=$PWD 
 
 [[ $_pwd == $HOME ]] || cd 
